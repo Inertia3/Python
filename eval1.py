75768a3832d98c6c53187f927cdb877106c896ee
Inertia3 <sahilansari37863@gmail.com>
2024-02-19 00:13:09 +0530

    Adding Evaluation file

---
+++ eval1.py
@@ -0,0 +1,33 @@
+def count_characters(input_string):
+    vowels = 0
+    consonants = 0
+    digits = 0
+    special_characters = 0
+
+
+    vowel_list = ['a','e','i','o','u','A','E','I','O','U']
+
+    for char in input_string:
+        if char.isalpha():
+            if char.lower() in vowel_list:
+                vowels += 1
+            else:
+                consonants += 1
+        elif char.isdigit():
+            digits += 1
+        else:
+            special_characters += 1
+
+    return vowels, consonants, digits, special_characters
+
+def main():
+    input_string = input("Enter a string: ")
+    vowel_count, consonant_count, digit_count, special_count = count_characters(input_string)
+
+    print("Vowels:", vowel_count)
+    print("Consonants:", consonant_count)
+    print("Digits:", digit_count)
+    print("Special Characters:", special_count)
+
+if __name__ == "__main__":
+    main()
