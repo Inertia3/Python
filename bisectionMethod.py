2c53194515c6cf320f14d440e08112b899437891
Inertia3 <sahilansari37863@gmail.com>
2024-02-18 17:33:27 +0530

    Commiting the file

---
+++ bisectionMethod.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def function(x):
+    ans = x*3 - x*2 - 1
+    return ans
+
+def bisectionMethod(a, b, n):
+    for i in range(n):
+        x = (a + b) / 2
+        if function(x) > 0:
+            b = x
+        else:
+            a = x
+    print("The root is:", x)
+
+
+def main():
+    a = float(input("First Approximation root: "))
+    b = float(input("Second Approximation root: "))
+    if function(a) * function(b) > 0:
+        print("Given approximate roots do not bracket the root.")
+        print("Try again with different approximate values.")
+    else:
+        n = int(input("Number of iterations: "))
+        bisectionMethod(a, b, n)
+
+if __name__ == "_main_":
+    main()
